44d4135495e371e4a482e99d51fe8a8379b0a877
Cesar Palo <alejandro2833@hotmail.com>
2022-12-19 12:12:00 -0500

    Update replica producto en página

--- src/main/java/com/tuini/babies/app/service/impl/ProductoEcommerceServiceImpl.java
+++ src/main/java/com/tuini/babies/app/service/impl/ProductoEcommerceServiceImpl.java
@@ -2,7 +2,11 @@ package com.tuini.babies.app.service.impl;
 
 import com.tuini.babies.app.model.Linea;
 import com.tuini.babies.app.model.ProductoEcommerce;
+import com.tuini.babies.app.model.ProductosPagina;
+import com.tuini.babies.app.model.VwProductosPagina;
 import com.tuini.babies.app.repository.ProductoEcommerceRepository;
+import com.tuini.babies.app.repository.ProductoPaginaRepository;
+import com.tuini.babies.app.repository.pub.ProductoEPaginaRepository;
 import com.tuini.babies.app.service.ProductoEcommerceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,12 @@ public class ProductoEcommerceServiceImpl implements ProductoEcommerceService {
 
     @Autowired
     private ProductoEcommerceRepository productoEcommerceRepository;
+
+    @Autowired
+    private ProductoEPaginaRepository productoEPaginaRepository;
+
+    @Autowired
+    private ProductoPaginaRepository productoPaginaRepository;
 
     @Override
     public List<ProductoEcommerce> getAll() {
@@ -55,6 +65,37 @@ public class ProductoEcommerceServiceImpl implements ProductoEcommerceService {
         upd.setEcom_precio_ant(producto.getEcom_precio_ant());
         upd.setUtilidad(producto.getUtilidad());
 
-        return this.productoEcommerceRepository.save(upd);
+        ProductoEcommerce ret = this.productoEcommerceRepository.save(upd);
+
+        VwProductosPagina prodVw = productoEPaginaRepository.getProductoPorId(producto.getCodigo());
+
+        Optional<ProductosPagina> opPp = this.productoPaginaRepository.findById(producto.getId());
+        if(opPp.orElse(null) == null)
+            return  producto;
+        ProductosPagina updPp = opPp.get();
+        updPp.setActivo(prodVw.getACTIVO());
+        updPp.setCodigo(prodVw.getCODIGO());
+        updPp.setNombre(prodVw.getNOMBRE());
+        updPp.setDescripcion(prodVw.getDESCRIPCION());
+        updPp.setEcommerce(prodVw.getECOMMERCE());
+        updPp.setPromocion(prodVw.getPROMOCION());
+        updPp.setNuevo(prodVw.getNUEVO());
+        updPp.setEcom_precio(prodVw.getECOM_PRECIO());
+        updPp.setEcom_precio_ant(prodVw.getECOM_PRECIO_ANT());
+        updPp.setEcom_img1(prodVw.getECOM_IMG1());
+        updPp.setEcom_img2(prodVw.getECOM_IMG2());
+        updPp.setEcom_img3(prodVw.getECOM_IMG3());
+//        updPp.setEcom_img4();
+//        updPp.setEcom_img5();
+        updPp.setMarca(prodVw.getMARCA());
+        updPp.setMarcaId(prodVw.getMARCA_ID().intValue());
+        updPp.setLinea(prodVw.getLINEA());
+        updPp.setLineaId(prodVw.getLINEA_ID().intValue());
+        updPp.setCategoria(prodVw.getCATEGORIA());
+        updPp.setCategoriaId(prodVw.getCATEGORIA_ID());
+
+        this.productoPaginaRepository.save(updPp);
+
+        return ret;
     }
 }
